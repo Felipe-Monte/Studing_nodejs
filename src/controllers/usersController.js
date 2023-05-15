ca320ed1062387be65d0b34fe1c66ed69ecf2121
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-15 20:46:11 -0300

    tratamento de excessões

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,15 @@
+const appError = require("../utils/appError")
 
-class UserController {
-  create(request, response) {
-    const { name, email, password } = request.body
+  class UserController {
+    create(request, response) {
+      const { name, email, password } = request.body
 
-    response.json({ name, email, password })
+      if (!name) {
+        throw new appError("Name is mandatory")
+      }
+
+      response.json({ name, email, password })
+    }
   }
-}
 
 module.exports = UserController
