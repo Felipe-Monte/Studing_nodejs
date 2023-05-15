16b3c0b52d998ab89a0969658ffc3702a088abe0
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-15 19:52:51 -0300

    testing middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,19 @@ const UserController = require("../controllers/usersController")
 
 const usersRoutes = Router()
 
+// Testing Middleware
 const usersController = new UserController()
 
-usersRoutes.post("/", usersController.create)
+function myMiddleware(request, response, next) {
+  console.log("passei pelo middleware")
+
+  if (!request.body.isAdmin) {
+    return response.json({ message: "user unauthorized" })
+  }
+
+  next()
+}
+
+usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
