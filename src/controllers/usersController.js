02c72c307ed64a5fbe9c1b16a0c4721343c8f848
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-05-17 22:10:29 -0300

    create database sql commands

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,15 +1,25 @@
+const { hash } = require("bcryptjs")
 const appError = require("../utils/appError")
 
-  class UserController {
-    create(request, response) {
-      const { name, email, password } = request.body
+const sqliteConnection = require('../database/sqlite')
 
-      if (!name) {
-        throw new appError("Name is mandatory")
-      }
+class UserController {
+  async create(request, response) {
+    const { name, email, password } = request.body
 
-      response.json({ name, email, password })
+    const database = await sqliteConnection()
+    const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if (checkUserExist) {
+      throw new appError("Este email já esta em uso !")
     }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run("INSERT INTO users  (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
+    return response.status(201).json()
   }
+}
 
 module.exports = UserController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,17 +6,6 @@ const usersRoutes = Router()
 
 const usersController = new UserController()
 
-// Testing Middleware
-function myMiddleware(request, response, next) {
-  console.log("passei pelo middleware")
-
-  if (!request.body.isAdmin) {
-    return response.json({ message: "user unauthorized" })
-  }
-
-  next()
-}
-
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
